83c3b9199cc90c20f2b7cfc11c8a993ee660bf99
iTom <44149820+iTomxy@users.noreply.github.com>
2024-10-07 23:26:39 +1100

    robustness of deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 
 set RIME=%APPDATA%\Rime
 set RIME_BASE=D:\Rime
-for /d %%d in ("%RIME_BASE%\*") do set RIME_BIN=%%d
+for /d %%d in ("%RIME_BASE%\weasel-*") do set RIME_BIN=%%d
 
 copy rytphings.*.yaml %RIME%
 "%RIME_BIN%\WeaselDeployer.exe" /deploy
