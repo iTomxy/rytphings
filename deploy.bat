b202b40f3d6078b9ed9e3b3b350ddac93f63c6ba
iTom <tianyou.liang@student.uts.edu.au>
2024-10-07 13:41:04 +1100

    update schema & deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,9 @@
 @echo off
 
 set RIME=%APPDATA%\Rime
-set RIME_BIN=D:\Rime\weasel-0.9.30
+set RIME_BIN=D:\Rime
+for /d %%d in ("%RIME_BASE%\*") do set RIME_BIN=%%d
 
 copy rytphings.*.yaml %RIME%
 "%RIME_BIN%\WeaselDeployer.exe" /deploy
+
