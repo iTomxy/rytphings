af7b080c2ad07865028e1042687755a70041d14d
iTom <44149820+iTomxy@users.noreply.github.com>
2025-10-09 22:28:35 +1100

    upd phrase & introduction.html deploy.bat: rm empty folder

--- deploy.bat
+++ deploy.bat
@@ -2,8 +2,8 @@
 
 set RIME=%APPDATA%\Rime
 set RIME_BASE=D:\Rime
+for /d %%d in ("%RIME_BASE%\weasel-*") do rmdir %%d > nul 2>&1
 for /d %%d in ("%RIME_BASE%\weasel-*") do set RIME_BIN=%%d
 
 copy rytphings.*.yaml %RIME%
 "%RIME_BIN%\WeaselDeployer.exe" /deploy
-
